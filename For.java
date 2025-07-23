4b2f7b2977ec6bcdab89d3767549271a7536d0c0
Tanishka Sain <133621980+Tanishka20102004@users.noreply.github.com>
2025-07-23 14:18:41 +0530

    For.java

---
+++ For.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class For {
+    public static void main(String[] args) {
+        for( int i=0; i<11; i++){
+            // System.out.println("I am the best");
+            System.out.println(i); // i+"  ----this for print in one line"
+        }
+    }
+}
